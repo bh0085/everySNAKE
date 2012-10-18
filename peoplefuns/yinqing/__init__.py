012fed8a53d553fd62127044c6703c9d17d4812b
ben <bh0085@gmail.com>
2012-10-18 15:52:30 -0400

    code for newline removal

--- peoplefuns/yinqing/__init__.py
+++ peoplefuns/yinqing/__init__.py
@@ -1,5 +1,6 @@
 import gzip, os
 
+#swap in your own path for data here or just create /data/yinqing
 path = "/data/yinqing/"
 fname = os.path.join(path, "mouse_chr1.fa.gz")
 fopen = gzip.open(fname)
@@ -8,3 +9,4 @@ for i in range(100):
     content = fopen.read(size = 10000)
     #read back 1000
     fopen.seek(fopen.tell() - 1000)
+    "".join(content.splitlines())
